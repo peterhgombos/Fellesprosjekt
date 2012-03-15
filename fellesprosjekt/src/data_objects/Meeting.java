3031ec748e24e24d4e162854ec4c9be099705741
Marte <Marte@78.91.7.69>
2012-03-15 12:18:42 +0100

    Getters, setters og variabler i Meeting og Appointment

--- fellesprosjekt/src/data_objects/Meeting.java
+++ fellesprosjekt/src/data_objects/Meeting.java
@@ -1,13 +1,21 @@
 package data_objects;
 
+import java.sql.Date;
 import java.util.HashMap;
 
 public class Meeting extends Appointment{
 	
-	//Integer er svaret fra person
-	private Room room;
-	private HashMap<Person, Integer> participants;
 	
+	private Room room;
+	//Integer er svaret fra person
+	private HashMap<Person, Integer> participants;
+	private int externalParticipants;
+	
+	public Meeting(String title, Date start, Date end, HashMap<Person, Integer> participants){
+		super(title, start, end);
+		this.participants = participants;
+	}
+
 	public Room getRoom() {
 		return room;
 	}
@@ -20,6 +28,7 @@ public class Meeting extends Appointment{
 		return participants;
 	}
 	
+	//Sjekke om deltakeren er med fra før?
 	public void addParticipant(Person person, int answear) {
 		participants.put(person, answear);
 	}
@@ -28,7 +37,13 @@ public class Meeting extends Appointment{
 		participants.remove(person);
 	}
 	
+	public int getParticipantsCount(){
+		return participants.size() + externalParticipants;
+	}
 	
+	public void changeCountExternalParticipants(int num){
+		this.externalParticipants = num;
+	}
 	
 	
 }
