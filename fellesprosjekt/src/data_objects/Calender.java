0febe472bd2e6051b1b7522076ec9d17f1889b95
marigjeiloweberg <marigjeiloweberg@dhcp-057079.wlan.ntnu.no>
2012-03-15 12:14:18 +0100

    Calender objektet er opprettet. Med kontstruktor og noen metoder

--- fellesprosjekt/src/data_objects/Calender.java
+++ fellesprosjekt/src/data_objects/Calender.java
@@ -1,5 +1,32 @@
 package data_objects;
 
-public class Calender {
+import java.util.ArrayList;
 
+public class Calender {
+	
+	private ArrayList<Appointment> appointments;
+	private ArrayList<Meeting> meetings;
+	private int weeknumber;
+	
+	public Calender(ArrayList<Appointment> appointments, ArrayList<Meeting> meetings, int weeknumber) {
+		this.appointments = appointments;
+		this.meetings = meetings;
+		this.weeknumber = weeknumber;
+	}
+	
+	public int nextWeek(){
+		return weeknumber+1;
+	}
+	
+	public int lastWeek(){
+		return weeknumber-1;
+	}
+	
+	public void addAppointments(Appointment appointment){
+		appointments.add(appointment);
+	}
+	
+	public void addMeetings(Meeting meeting){
+		meetings.add(meeting);
+	}
 }
