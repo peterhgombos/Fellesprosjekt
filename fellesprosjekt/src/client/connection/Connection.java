b70bc8f72d53834207c056c05ecf92cd0e35fe6b
Erlend Sigholt <erlendsi@stud.ntnu.no>
2012-03-15 15:10:21 +0100

    Methods for Connection

--- fellesprosjekt/src/client/connection/Connection.java
+++ fellesprosjekt/src/client/connection/Connection.java
@@ -1,19 +1,19 @@
 package client.connection;
 
 import java.io.IOException;
-import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.Socket;
-import java.net.SocketAddress;
-import java.util.HashMap;
+import java.util.LinkedList;
 
 import utilities.Console;
+import dataobjects.*;
 
-public class Connection {
+public class Connection  {
 
 	private static Socket socket;
 	public static Console console;
 	public static ServerWriter writer;
+	LinkedList<ConnectionListener> listeners;
 
 	public static void connect() throws IOException {
 		console = new Console();
@@ -25,7 +25,53 @@ public class Connection {
 		writer.send("melding");
 	}
 	
+	public void addConnectionListener(ConnectionListener listener) {
+		this.listeners.add(listener);
+	}
+	
+	public void removeConnectionListener(ConnectionListener listener) {
+		this.removeConnectionListener(listener);
+	}
+	
+	
+	public static void receiveMessage(String s) {
+		//TODO
+	}
+	
+	public void requestPersonLoggedIn(String username) {
+		//TODO
+	}
+	
+	public void login(String username, String password) {
+		//TODO
+	}
+	
+	public void requestAppointments(Person p) {
+		//TODO
+	}
+	
+	public void requestMeetings(Person p) {
+		//TODO
+	}
+	
+	public void requestLeader(Appointment a) {
+		//TODO
+	}
+	
+	public void requestParticipants(Meeting m) {
+		//TODO
+	}
+	
+	public void requestMessages(Person p) {
+		//TODO
+	}
+	
+	public void requestRoom(Meeting m) {
+		//TODO
+	}
+	
 	public static void main(String[] args) throws IOException {
 		Connection.connect();
 	}
+
 }
--- fellesprosjekt/src/client/connection/ServerReader.java
+++ fellesprosjekt/src/client/connection/ServerReader.java
@@ -3,10 +3,7 @@ package client.connection;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.Reader;
 import java.net.Socket;
-
-import server.ServerConstants;
 
 public class ServerReader extends Thread {
 	
@@ -44,7 +41,7 @@ public class ServerReader extends Thread {
 					return;
 				}
 				
-				Connection.console.writeline(message);
+				Connection.receiveMessage(message);
 				
 			} catch (IOException e) {
 				e.printStackTrace();
