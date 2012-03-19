bf67216467dd28bfbacd794552b23cf39fe64c43
marigjeiloweberg <marigjeiloweberg@dhcp-058244.wlan.ntnu.no>
2012-03-19 10:45:54 +0100

    toXML for meeting ferdig

--- fellesprosjekt/src/dataobjects/Meeting.java
+++ fellesprosjekt/src/dataobjects/Meeting.java
@@ -67,6 +67,7 @@ public class Meeting extends Appointment{
 		xml.append(XMLElements.openXML(XMLElements.ROOM) + getRoom() + XMLElements.closeXML(XMLElements.ROOM));
 		xml.append(XMLElements.openXML(XMLElements.PLACE) + getPlace() + XMLElements.closeXML(XMLElements.PLACE));
 		xml.append(XMLElements.openXML(XMLElements.PARTICIPANT) + " " + XMLElements.closeXML(XMLElements.PARTICIPANT));
+		xml.append(XMLElements.closeXML(XMLElements.MEETING));
 		return xml.toString();
 	}
 	
