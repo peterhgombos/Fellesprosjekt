9d84dc6388cc68903c5cbe5550ebca80644e61b3
gombos <gombos@stud.ntnu.no>
2012-03-19 12:02:11 +0100

    added static method to get SHA-1 hash of given string

---
+++ fellesprosjekt/src/client/authentication/SHAHasher.java
@@ -0,0 +1,25 @@
+package client.authentication;
+
+import java.security.*;
+
+public class SHAHasher {
+	public static String SHAHash(String id){
+		try {
+			MessageDigest algorithm = MessageDigest.getInstance("SHA");
+			algorithm.reset();
+			algorithm.update(id.getBytes());
+			byte messageDigest[] = algorithm.digest();
+			StringBuffer hexString = new StringBuffer();
+			for (int i = 0; i < messageDigest.length; i++) {
+				hexString.append(Integer.toHexString(0xFF & messageDigest[i]));
+			}
+			return hexString.toString();
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return "";
+		}
+		
+	}
+	
+}
