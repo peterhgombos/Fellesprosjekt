acd507e829abc99e065b571797d05b329a5ad89b
Marthe <marthbek@stud.ntnu.no>
2012-03-21 16:42:58 +0100

    ErrorPane

--- fellesprosjekt/src/client/gui/UserInformationMessages.java
+++ fellesprosjekt/src/client/gui/UserInformationMessages.java
@@ -5,12 +5,12 @@ import javax.swing.JOptionPane;
 public class UserInformationMessages  {
 	
 	//av typen; melding sendt
-	public void showMessage(String message){
+	public static void showMessage(String message){
 		JOptionPane.showMessageDialog(null, message);
 	}
 	
 	//av typen; du har ikke skrevet inn passord
-	public void showErrormessage(String message){
-		JOptionPane.showMessageDialog(null, message);
+	public static void showErrormessage(String message){
+		JOptionPane.showMessageDialog(null, message,"FEIL",JOptionPane.ERROR_MESSAGE);
 	}
 }
