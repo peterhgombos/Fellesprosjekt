4df4714db2dc79b1d66a9334f2b7f260930e8c13
Péter Gombos <gombos@stud.ntnu.no>
2012-03-21 14:15:29 +0100

    fixed a bit in note

--- fellesprosjekt/src/common/dataobjects/Note.java
+++ fellesprosjekt/src/common/dataobjects/Note.java
@@ -7,19 +7,13 @@ public class Note {
 	private int id;
 	private String title;
 	private Date timeAdded;
-	private String text;
 	private Appointment appointment;
 	
-	public Note (String title, Date timeAdded, int id, String text, Appointment appointment){
+	public Note (String title, Appointment appointment){
 		this.title = title;
 		this.timeAdded = timeAdded;
 		this.id = id;
-		this.text = text;
 		this.appointment = appointment;
-	}
-	
-	public String getText() {
-		return text;
 	}
 	
 	public Appointment getAppointment() {
