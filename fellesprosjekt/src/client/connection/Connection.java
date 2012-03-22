76de7e8fcae04eb59eb6e139690263864570b06c
Marte <Marte@78.91.7.108>
2012-03-22 16:41:00 +0100

    kjhjkh

--- fellesprosjekt/src/client/connection/Connection.java
+++ fellesprosjekt/src/client/connection/Connection.java
@@ -56,7 +56,6 @@ public class Connection  {
 	
 	
 	public void requestMeetingsAndAppointments(Person p) {
-		System.out.println(p);
 		writer.send(new ComMessage(p, MessageType.REQUEST_APPOINTMENTS_AND_MEETINGS));
 	}
 	
