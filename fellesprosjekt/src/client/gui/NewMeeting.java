312c9fe9fa92ee0955253b044a55f2a003ccb234
marigjeiloweberg <marigjeiloweberg@dhcp-012148.wlan.ntnu.no>
2012-03-23 11:41:11 +0100

    ingen ting

--- fellesprosjekt/src/client/gui/NewMeeting.java
+++ fellesprosjekt/src/client/gui/NewMeeting.java
@@ -5,7 +5,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
-import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
@@ -15,7 +14,6 @@ import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
-import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
@@ -23,20 +21,18 @@ import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
-import javax.swing.WindowConstants;
+
 
 import com.toedter.calendar.JDateChooser;
 
 import client.Client;
 import client.connection.ServerData;
 
-import common.dataobjects.Appointment;
+
 import common.dataobjects.Meeting;
-import common.dataobjects.Note;
 import common.dataobjects.Person;
 import common.utilities.DateString;
 
-import server.Server;
 
 public class NewMeeting extends JPanel{
 	
