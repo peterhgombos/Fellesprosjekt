1b7b5dd9e8cb5dcdf5b06541b20fc0db2b53cc67
Péter Gombos <gombos@stud.ntnu.no>
2012-03-26 15:51:37 +0200

    fixed bug for real in new appointment

--- fellesprosjekt/src/client/gui/NewAppointment.java
+++ fellesprosjekt/src/client/gui/NewAppointment.java
@@ -88,6 +88,7 @@ public class NewAppointment extends JPanel{
 		datepicker.setMinSelectableDate(new Date(System.currentTimeMillis()));
 		endDPicker = new JDateChooser();
 		endDPicker.setDate(datepicker.getDate());
+		endDPicker.setEnabled(false);
 		endDPicker.setVisible(false);
 		endDPicker.setMinSelectableDate(new Date(System.currentTimeMillis()));
 		severalDaysLabel = new JLabel("Flere dager");
