246fdbb250868a6c823a3b27d7e41b301af77308
Erlend Sigholt <erlendsi@stud.ntnu.no>
2012-03-26 13:09:09 +0200

    Support for appointments over several weeks

--- fellesprosjekt/src/common/dataobjects/InternalCalendar.java
+++ fellesprosjekt/src/common/dataobjects/InternalCalendar.java
@@ -8,11 +8,11 @@ import java.util.HashMap;
 import java.util.TimeZone;
 
 public class InternalCalendar {
-	
+
 	private GregorianCalendar calendar;
-	
+
 	private HashMap<Integer, HashMap<Integer, ArrayList<Appointment>[]>> apps;
-	
+
 	public InternalCalendar() {
 		reset();
 	}
@@ -20,49 +20,66 @@ public class InternalCalendar {
 		calendar = new GregorianCalendar();
 		calendar.setTimeZone(TimeZone.getDefault());
 		calendar.setMinimalDaysInFirstWeek(4);
-		
+
 		calendar.setFirstDayOfWeek(Calendar.MONDAY);
 		calendar.setTimeInMillis(System.currentTimeMillis());
-		
+
 		apps = new HashMap<Integer, HashMap<Integer,ArrayList<Appointment>[]>>();
 	}
-	
-	
+
+
 	@SuppressWarnings("unchecked")
 	private void addToCal(Appointment app){
-		
+
 		calendar.set(app.getStartTime().getYear(), app.getStartTime().getMonth() -1, app.getStartTime().getDay(), app.getStartTime().getHour(), 0);
 		int year = calendar.get(Calendar.YEAR);
 		int week = calendar.get(Calendar.WEEK_OF_YEAR);
 		int day = dayToWeekDay(calendar.get(Calendar.DAY_OF_WEEK));
 		int hourInWeek = day * 24 + calendar.get(Calendar.HOUR_OF_DAY);
-		
+
 		calendar.set(app.getEndTime().getYear(), app.getEndTime().getMonth() -1, app.getEndTime().getDay(), app.getEndTime().getHour(), 0);
+		int eYear = calendar.get(Calendar.YEAR);
 		int eDay = dayToWeekDay(calendar.get(Calendar.DAY_OF_WEEK));
+		int eWeek = calendar.get(Calendar.WEEK_OF_YEAR);
 		int eHourInWeek = eDay * 24 + calendar.get(Calendar.HOUR_OF_DAY);
+
+
 		
-		HashMap<Integer, ArrayList<Appointment>[]> tempyear = apps.get(year);
-		if(tempyear == null){
-			tempyear = new HashMap<Integer, ArrayList<Appointment>[]>();
-			apps.put(year, tempyear);
-		}
+		int weeks = 1;
+		weeks += eWeek - week;
 		
-		ArrayList<Appointment>[] arr = tempyear.get(week);
-		if(arr == null){
-			arr = (ArrayList<Appointment>[]) new ArrayList[24*7];
-			tempyear.put(week, arr);
-		}else {
-			tempyear.put(week, arr);
-		}
-		
-		for(int i = hourInWeek; i < eHourInWeek; i++){
-			if(arr[i] == null){
-				arr[i] = new ArrayList<Appointment>(1);
+		for (int j = 0; j < weeks; j++) {
+			int start = j == 0 ? hourInWeek : 0;
+			int slutt = j == (weeks -1) ? eHourInWeek: 24*7;
+
+			calendar.set(Calendar.WEEK_OF_YEAR, week + j);
+
+			int curryear = calendar.get(Calendar.YEAR);
+			HashMap<Integer, ArrayList<Appointment>[]> tempyear = apps.get(curryear);
+			if(tempyear == null){
+				tempyear = new HashMap<Integer, ArrayList<Appointment>[]>();
+				apps.put(curryear, tempyear);
 			}
-			arr[i].add(app);
+
+			int currweek =  calendar.get(Calendar.WEEK_OF_YEAR);
+			ArrayList<Appointment>[] arr = tempyear.get(currweek);
+			if(arr == null){
+				arr = (ArrayList<Appointment>[]) new ArrayList[24*7];
+				tempyear.put(currweek, arr);
+			}else {
+				tempyear.put(currweek, arr);
+			}
+
+
+			for(int i = start; i < slutt; i++) {
+				if(arr[i] == null){
+					arr[i] = new ArrayList<Appointment>(1);
+				}
+				arr[i].add(app);
+			}
 		}
 	}
-	
+
 	public int dayToWeekDay(int day){
 		if(day == Calendar.SATURDAY){
 			return 5;
@@ -71,7 +88,7 @@ public class InternalCalendar {
 		}
 		return day - 2;
 	}
-	
+
 	public ArrayList<Appointment> getAppointments(int year, int week, int day, int hour){
 		HashMap<Integer, ArrayList<Appointment>[]> tempyear = apps.get(year);
 		if(tempyear == null){
@@ -83,20 +100,20 @@ public class InternalCalendar {
 		}
 		return arr[day*24 + hour];
 	}
-	
+
 	public boolean startsInHour(Appointment a, int year, int week, int hour){
 		calendar.set(a.getStartTime().getYear(), a.getStartTime().getMonth() -1, a.getStartTime().getDay(), a.getStartTime().getHour(), 0);
 		int cyear = calendar.get(Calendar.YEAR);
 		int cweek = calendar.get(Calendar.WEEK_OF_YEAR);
 		int cday = dayToWeekDay(calendar.get(Calendar.DAY_OF_WEEK));
 		int chourInWeek = cday * 24 + calendar.get(Calendar.HOUR_OF_DAY);
-		
+
 		if(year == cyear && week == cweek && hour == chourInWeek){
 			return true;
 		}
 		return false;
 	}
-	
+
 	public String getDateForDay(int year, int week, int day){
 		int currentday = 1;
 		if(day == 1 ){
@@ -114,18 +131,18 @@ public class InternalCalendar {
 		}else if(day == 7 ){
 			currentday = Calendar.SUNDAY;
 		}
-		
+
 		calendar.set(Calendar.YEAR, year);
 		calendar.set(Calendar.WEEK_OF_YEAR, week);
 		calendar.set(Calendar.DAY_OF_WEEK, currentday);
-		
+
 		return " " +calendar.get(Calendar.DAY_OF_MONTH)+"."+ calendar.get(Calendar.MONTH);
 	}
-	
+
 	public void addAppointment(Appointment appointment){
 		addToCal(appointment);
 	}
-	
+
 	public void addMeeting(Meeting meeting){
 		addToCal(meeting);
 	}
